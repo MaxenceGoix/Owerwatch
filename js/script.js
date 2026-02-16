208ee4199ee190b6b038a29cb6df27e618f93c26
RomainChh <romain.chheam@efrei.net>
2026-02-16 18:15:57 +0100

    Remplissage et style des pages concernat les héros

--- js/script.js
+++ js/script.js
@@ -68,3 +68,13 @@ formulaire.addEventListener('submit', function(event) {
 etudiant.notes.forEach(note => {
     console.log("Note : " + note);
 });
+
+/* Affichage capacités */
+function toggleAbilities(id) {
+        var panel = document.getElementById(id);
+        if (panel.style.display === "none" || panel.style.display === "") {
+            panel.style.display = "grid";
+        } else {
+            panel.style.display = "none";
+        }
+    }
