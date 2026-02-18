fbe29b5b000f4f9a71c967b1768cf24f9943656a
RomainChh <romain.chheam@efrei.net>
2026-02-18 13:12:48 +0100

    ajout footer aux pages manquantes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* Affichage capacités */
 function toggleAbilities(id) {
-        var panel = document.getElementById(id);
+        let panel = document.getElementById(id);
         if (panel.style.display === "none" || panel.style.display === "") {
             panel.style.display = "grid";
         } else {
